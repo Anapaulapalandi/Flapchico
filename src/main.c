ca85d691d03767bea8e0e5a4925e1c49f06b54e3
apalandi <ana.palandi@capgemini.com>
2026-05-08 14:40:17 -0300

    Cano and Chico moving around

--- src/main.c
+++ src/main.c
@@ -1,76 +1,78 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include "raylib.h"
-#include "resource_dir.h"	
+#include "resource_dir.h"
 
-#define GRAVITY 50
+#define GRAVITY 500
 
 typedef struct Chico {
-	Vector2 pos;
-	float velocity;
-	float col_radius;
-}Chico;
+    Vector2 pos;
+    float velocity;
+    float col_radius;
+} Chico;
 
 typedef struct Pipe {
-	Vector2 pos;
-	float velocity;
-	int hg;
-	int wt;
-}Pipe;
+    Vector2 pos;
+    float velocity;
+    int hg;
+    int wt;
+} Pipe;
 
 Chico player;
-Pipe test;
+Pipe pipe;
 
-int main ()
-{
+int main() {
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    InitWindow(800, 600, "Play Flapchico");
+    SearchAndSetResourceDir("resources");
 
-	//Setup raylib//
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-	InitWindow(800, 600, "Play Flapchico");
-	SearchAndSetResourceDir("resources");
+    player.pos.x = 400;
+    player.pos.y = 300;
+    player.velocity = 0;
 
-	//game init//
-	player.pos.x = 400;
-	player.pos.y = 300;
+    pipe.pos.x = 600;
+    pipe.pos.y = 300;
+    pipe.wt = 64;
+    pipe.hg = 128;
+    pipe.velocity = 35;
 
-	test.pos.x = 600;
-	test.pos.y = 300;
-	test.wt = 64;
-	test.hg = 128;
-	test.velocity = 35;
+    Texture2D chicoTex = LoadTexture("Chicao.png");
+    Texture2D pipeTex = LoadTexture("pipe.png");
 
-	//Texture chico = LoadTexture("Chicao.png");
-	Texture2D chico = LoadTexture("Chicao.png");
-	
-	
-	while (!WindowShouldClose())		
-	{
-		//velocity
-		float dt = GetFrameTime();
-		player.velocity += GRAVITY*dt;
-		player.pos.y += player.velocity*dt;
+    // ✅ Verifica se carregou
+    if (pipeTex.id == 0) {
+        TraceLog(LOG_ERROR, "FALHOU ao carregar pipe.png");
+    }
 
-		//pipe walk
-		test.pos.x -= test.velocity * dt;
+    while (!WindowShouldClose()) {
+        float dt = GetFrameTime();
 
-		//drawing
-		BeginDrawing();
+        player.velocity += GRAVITY * dt;
+        player.pos.y += player.velocity * dt;
+        pipe.pos.x -= pipe.velocity * dt;
 
-		
-		ClearBackground(WHITE);
+        if (IsKeyPressed(KEY_SPACE)) {
+            player.velocity = -300.0f;
+        }
 
-		DrawRectangle(test.pos.x, test.pos.y, test.wt, test.hg, YELLOW);
-		//DrawTexture(chico, player.pos.x,player.pos.y, GREEN);
-		DrawTextureEx(chico, player.pos, 0, 32.0f / chico.width, WHITE);
-		
-		
-		EndDrawing();
-	}
+        BeginDrawing();
+        ClearBackground(WHITE);
 
-	
-	UnloadTexture(chico);
+        // 🟩 Retângulo debug - se aparecer verde o problema é na textura
+        DrawRectangle(pipe.pos.x, pipe.pos.y, pipe.wt, pipe.hg, GREEN);
 
-	
-	CloseWindow();
-	return 0;
+        // Textura do pipe com scale proporcional
+        float pipeScale = (float)pipe.wt / pipeTex.width;
+        DrawTextureEx(pipeTex, pipe.pos, 0, pipeScale, WHITE);
+
+        DrawTextureEx(chicoTex, player.pos, 0, 32.0f / chicoTex.width, WHITE);
+
+        EndDrawing();
+    }
+
+    UnloadTexture(chicoTex);
+    UnloadTexture(pipeTex);
+
+    CloseWindow();
+    return 0;
 }
